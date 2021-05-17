a5d01be49b4e993cd4f2ea297ac42a724ee0028c
Ernane Ferreira <ernane.junior25@gmail.com>
2021-05-16 23:27:15 -0300

    :iphone: responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
-getUser("ernanej");
+// getUser("ernanej");
 
 async function getUser(username) {
     const resp = await fetch(APIURL + username);
